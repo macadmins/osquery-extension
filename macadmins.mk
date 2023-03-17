055ddf297abf1b51d0a481316be4370f05dc1c34
Erik Gomez <erikg@uber.com>
2023-03-17 13:59:46 -0500

    first attempt at github actions for osquery extension

---
+++ macadmins.mk
@@ -0,0 +1,4 @@
+APPLE_ACC_PWD=
+APPLE_ACC_USER=
+DEV_APP_CERT=Developer ID Application: Mac Admins Open Source (T4SK8ZXCXG)
+DEV_INSTALL_CERT=Developer ID Installer: Mac Admins Open Source (T4SK8ZXCXG)
