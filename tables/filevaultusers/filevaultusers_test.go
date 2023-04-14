d36f9b73e0a340ae1e35b41c6c02c1629688b837
Graham Gilbert <graham.gilbert@airbnb.com>
2023-04-14 14:00:11 -0700

    handle multi line

--- tables/filevaultusers/filevaultusers_test.go
+++ tables/filevaultusers/filevaultusers_test.go
@@ -35,3 +35,20 @@ func TestProcessFDESetupToUsersWithIncorrectInput(t *testing.T) {
 		t.Error("processFDESetupToUsers did not error when passed a malformed input")
 	}
 }
+
+func TestProcessFDESetupToUsersWithMultilineInput(t *testing.T) {
+	t.Parallel()
+	inputBytes := []byte("graham,163DDC62-5D23-40A2-8EC9-0190B267251B\ndave,A643042D-6F7C-4A87-9EDB-2CA267035B01")
+	var expectedOutput []FileVaultUser
+	expectedOutput = append(expectedOutput, FileVaultUser{Username: "graham", UUID: "163DDC62-5D23-40A2-8EC9-0190B267251B"})
+	expectedOutput = append(expectedOutput, FileVaultUser{Username: "dave", UUID: "A643042D-6F7C-4A87-9EDB-2CA267035B01"})
+
+	output, err := processFDESetupToUsers(inputBytes)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !reflect.DeepEqual(expectedOutput, output) {
+		t.Error(cmp.Diff(expectedOutput, output))
+	}
+}
