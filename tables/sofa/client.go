4ca484aaf85714542fc9a10a6c3d0db62cf4b5c8
Graham Gilbert <graham.gilbert@airbnb.com>
2024-05-21 18:34:34 -0700

    Move to using the etag

---
+++ tables/sofa/client.go
@@ -0,0 +1,270 @@
+package sofa
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"os"
+	"path"
+	"strings"
+	"time"
+
+	"github.com/macadmins/osquery-extension/pkg/utils"
+)
+
+const SofaV1URL = "https://sofa.macadmins.io/v1/macos_data_feed.json"
+
+type SofaClient struct {
+	endpoint   string
+	httpClient *http.Client
+	localEtag  string
+	remoteEtag string
+	cacheFile  string
+	cacheDir   string
+	etagFile   string
+}
+
+type SofaTime time.Time
+
+func (t SofaTime) String() string {
+	return time.Time(t).String()
+}
+
+func (t SofaTime) MarshalJSON() ([]byte, error) {
+	return []byte(`"` + time.Time(t).UTC().Format("2006-01-02T15:04:05Z") + `"`), nil
+}
+
+func (t *SofaTime) UnmarshalJSON(b []byte) (err error) {
+	s := strings.Trim(string(b), "\"")
+	// Remove trailing 'Z' to handle fixed timezone offset
+	s = strings.TrimSuffix(s, "Z")
+
+	if s == "" {
+		return nil // Handle empty time string
+	}
+
+	// Manually parse the time string with timezone offset
+	parsedTime, err := time.Parse("2006-01-02T15:04:05-07:00", s)
+	if err != nil {
+		return err
+	}
+	*t = SofaTime(parsedTime.UTC())
+	return nil
+}
+
+func WithLocalCache(cacheFile, etagFile string) Option {
+	return func(s *SofaClient) {
+		s.cacheFile = cacheFile
+		s.etagFile = etagFile
+	}
+}
+
+func WithCacheDir(cacheDir string) Option {
+	return func(s *SofaClient) {
+		s.cacheDir = cacheDir
+	}
+}
+
+func WithURL(url string) Option {
+	return func(s *SofaClient) {
+		s.endpoint = url
+	}
+}
+
+func WithHTTPClient(client *http.Client) Option {
+	return func(s *SofaClient) {
+		s.httpClient = client
+	}
+}
+
+func NewSofaClient(opts ...Option) (*SofaClient, error) {
+
+	s := &SofaClient{
+		endpoint: SofaV1URL,
+		httpClient: &http.Client{
+			Timeout: 10 * time.Second,
+		},
+		cacheDir: "/private/tmp/sofa",
+	}
+
+	for _, opt := range opts {
+		opt(s)
+	}
+
+	err := s.createCacheDir()
+	if err != nil {
+		return nil, err
+	}
+
+	s.setCachePaths()
+
+	return s, nil
+}
+
+func (s *SofaClient) setCachePaths() {
+	if s.etagFile == "" {
+		s.etagFile = s.etagPath("macos_data_feed.json.json")
+	}
+
+	if s.cacheFile == "" {
+		s.cacheFile = s.cachePath("macos_data_feed_etag.txt")
+	}
+}
+
+func (s *SofaClient) cachePath(fileName string) string {
+	return path.Join(s.cacheDir, fileName)
+}
+
+func (s *SofaClient) etagPath(fileName string) string {
+	return path.Join(s.cacheDir, fileName)
+}
+
+func (s *SofaClient) createCacheDir() error {
+
+	err := os.MkdirAll(s.cacheDir, 0755)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *SofaClient) cacheValid() (bool, error) {
+	if !utils.FileExists(s.cacheFile) {
+		return false, nil
+	}
+
+	remoteEtag, err := s.getEtag()
+	if err != nil {
+		return false, err
+	}
+
+	localEtag, err := s.loadCachedEtag()
+	if err != nil {
+		return false, err
+	}
+
+	s.localEtag = localEtag
+	s.remoteEtag = remoteEtag
+
+	err = s.saveEtag()
+	if err != nil {
+		return false, err
+	}
+
+	if s.localEtag == s.remoteEtag {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func (s *SofaClient) loadCachedData() (Root, error) {
+
+	jsonData, err := os.ReadFile(s.cacheFile)
+	if err != nil {
+		return Root{}, err
+	}
+
+	var root Root
+	if err := json.Unmarshal(jsonData, &root); err != nil {
+		return Root{}, err
+	}
+
+	return root, nil
+
+}
+
+func (s *SofaClient) downloadData() error {
+	return s.downloadFile(s.endpoint, s.cacheFile)
+}
+
+func (s *SofaClient) loadCachedEtag() (string, error) {
+	if !utils.FileExists(s.etagFile) {
+		return "", nil
+	}
+
+	etagData, err := os.ReadFile(s.etagFile)
+	if err != nil {
+		return "", err
+	}
+
+	return string(etagData), nil
+}
+
+func (s *SofaClient) getEtag() (string, error) {
+	req, err := http.NewRequest("HEAD", s.endpoint, http.NoBody)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := s.httpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close() // nolint: errcheck
+
+	etag := resp.Header.Get("ETag")
+	if etag == "" {
+		return "", errors.New("etag not found in response")
+	}
+
+	return etag, nil
+}
+
+func (s *SofaClient) saveEtag() error {
+	return os.WriteFile(s.etagFile, []byte(s.remoteEtag), 0644)
+}
+
+func (s *SofaClient) downloadFile(url, path string) error {
+	req, err := http.NewRequest("GET", url, http.NoBody)
+	if err != nil {
+		return err
+	}
+
+	resp, err := s.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close() // nolint: errcheck
+
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close() // nolint: errcheck
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	etag := resp.Header.Get("ETag")
+	if etag != "" {
+		s.remoteEtag = etag
+		err = s.saveEtag()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *SofaClient) downloadSofaJSON() (Root, error) {
+	valid, err := s.cacheValid()
+	if err != nil {
+		return Root{}, err
+	}
+
+	if valid {
+		return s.loadCachedData()
+	}
+
+	err = s.downloadData()
+	if err != nil {
+		return Root{}, err
+	}
+
+	return s.loadCachedData()
+}
---
+++ tables/sofa/client_test.go
@@ -0,0 +1,340 @@
+package sofa
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+	"time"
+
+	_ "embed"
+
+	"github.com/macadmins/osquery-extension/pkg/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+//go:embed test_data.json
+var testData []byte
+
+//go:embed test_etag.txt
+var testEtag []byte
+
+//go:embed test_invalid_etag.txt
+var testInvalidEtag []byte
+
+const (
+	testCacheFile = "testCache.json"
+	testEtagFile  = "testetag.txt"
+)
+
+func setupTestServer() *httptest.Server {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		etag := `W/"123456789"`
+		w.Header().Set("ETag", etag)
+		w.Write(testData) //nolint:errcheck
+	})
+
+	server := httptest.NewServer(handler)
+
+	return server
+}
+
+func setup() (server *httptest.Server) {
+	server = setupTestServer()
+	return server
+}
+
+func teardown(server *httptest.Server) {
+	server.Close()
+	removeTestFiles()
+}
+
+func removeTestFiles() {
+	os.Remove(testCacheFile)
+	os.Remove(testEtagFile)
+}
+
+func TestDownloadFile(t *testing.T) {
+	server := setup()
+	defer teardown(server)
+
+	client, err := NewSofaClient(
+		WithURL(server.URL),
+	)
+
+	assert.NoError(t, err)
+
+	// Define a temporary file path for the test
+	tempFile := "temp.txt"
+
+	// Call the method under test
+	err = client.downloadFile(server.URL, tempFile)
+
+	// Assert that no error occurred
+	assert.NoError(t, err)
+
+	// Check that the file was created
+	_, err = os.Stat(tempFile)
+	assert.NoError(t, err)
+
+	// Clean up the temporary file
+	err = os.Remove(tempFile)
+	assert.NoError(t, err)
+}
+
+func TestUnmarshalJSON(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected time.Time
+	}{
+		{
+			name:     "Valid Time String",
+			input:    `"2024-05-06T17:02:57+00:00Z"`,
+			expected: time.Date(2024, 5, 6, 17, 2, 57, 0, time.UTC),
+		},
+		{
+			name:     "Empty Time String",
+			input:    `""`,
+			expected: time.Time{},
+		},
+		// Add more test cases as needed
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			var st SofaTime
+			err := json.Unmarshal([]byte(tc.input), &st)
+			assert.NoError(t, err)
+
+			actual := time.Time(st)
+			t.Logf("Actual value: %v", actual) // Print the actual value for debugging
+
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestLoacCachedEtag(t *testing.T) {
+	// Create a temporary file
+	tempEtagFile, err := os.CreateTemp("", "test")
+	assert.NoError(t, err)
+	defer os.Remove(tempEtagFile.Name()) // clean up
+
+	// Write the etag to the etag file
+	err = os.WriteFile(tempEtagFile.Name(), testEtag, 0644)
+	assert.NoError(t, err)
+
+	// Create a SofaClient
+	client := &SofaClient{
+		etagFile: tempEtagFile.Name(),
+	}
+
+	// Call the method under test
+	etag, err := client.loadCachedEtag()
+	assert.NoError(t, err)
+
+	// Assert that the etag was loaded correctly
+	assert.Equal(t, "W/\"123456789\"", etag)
+}
+
+func TestGetEtag(t *testing.T) {
+	server := setup()
+	defer teardown(server)
+
+	// Create a SofaClient
+	client := &SofaClient{
+		endpoint:   server.URL,
+		httpClient: http.DefaultClient,
+	}
+
+	// Call the method under test
+	etag, err := client.getEtag()
+	assert.NoError(t, err)
+
+	// Assert that the etag was returned correctly
+	assert.Equal(t, "W/\"123456789\"", etag)
+}
+
+func TestSaveEtag(t *testing.T) {
+	// Create a temporary file
+	tempEtagFile, err := os.CreateTemp("", "test")
+	assert.NoError(t, err)
+	defer os.Remove(tempEtagFile.Name()) // clean up
+
+	// Create a SofaClient
+	client := &SofaClient{
+		etagFile:   tempEtagFile.Name(),
+		remoteEtag: "W/\"123456789\"",
+		httpClient: http.DefaultClient,
+	}
+
+	// Call the method under test
+	err = client.saveEtag()
+	assert.NoError(t, err)
+
+	// Read the etag file
+	etagData, err := os.ReadFile(client.etagFile)
+	assert.NoError(t, err)
+
+	// Assert that the etag was saved correctly
+	assert.Equal(t, "W/\"123456789\"", string(etagData))
+}
+
+func TestLoadCachedData(t *testing.T) {
+	// Create a temporary file
+	tempCacheFile, err := os.CreateTemp("", "test")
+	assert.NoError(t, err)
+	defer os.Remove(tempCacheFile.Name()) // clean up
+
+	// Write the data to the cache file
+	err = os.WriteFile(tempCacheFile.Name(), testData, 0644)
+	assert.NoError(t, err)
+
+	// Create a SofaClient
+	client := &SofaClient{
+		cacheFile: tempCacheFile.Name(),
+	}
+
+	var expectedRoot Root
+	err = json.Unmarshal(testData, &expectedRoot)
+	assert.NoError(t, err)
+
+	// Call the method under test
+	data, err := client.loadCachedData()
+	assert.NoError(t, err)
+
+	// Assert that the data was loaded correctly
+	assert.Equal(t, expectedRoot, data)
+}
+
+func TestCacheValid(t *testing.T) {
+	server := setup()
+	defer teardown(server)
+	// Create a temporary file
+	tempCacheFile, err := os.CreateTemp("", "test")
+	assert.NoError(t, err)
+	defer os.Remove(tempCacheFile.Name()) // clean up
+
+	tempEtagFile, err := os.CreateTemp("", "test")
+	assert.NoError(t, err)
+	defer os.Remove(tempEtagFile.Name()) // clean up
+
+	// Create a SofaClient
+	client := &SofaClient{
+		endpoint:   server.URL,
+		cacheFile:  tempCacheFile.Name(),
+		etagFile:   tempEtagFile.Name(),
+		httpClient: http.DefaultClient,
+	}
+
+	// Download the data files
+	err = client.downloadData()
+	assert.NoError(t, err)
+
+	// Write the etag to the etag file
+	err = os.WriteFile(client.etagFile, testEtag, 0644)
+	assert.NoError(t, err)
+
+	// Call the method under test
+	valid, err := client.cacheValid()
+	// Assert that no error occurred
+	assert.NoError(t, err)
+
+	// Assert that the cache is valid
+	assert.True(t, valid)
+
+}
+
+func TestDownloadData(t *testing.T) {
+	server := setup()
+	defer teardown(server)
+
+	tmpDir := os.TempDir()
+
+	// Create a SofaClient
+	client := &SofaClient{
+		endpoint:   server.URL,
+		httpClient: http.DefaultClient,
+		cacheDir:   tmpDir,
+	}
+
+	err := client.createCacheDir()
+	assert.NoError(t, err)
+
+	client.setCachePaths()
+
+	// Call the method under test
+	err = client.downloadData()
+
+	// Assert that no error occurred
+	assert.NoError(t, err)
+
+	assert.True(t, utils.FileExists(client.cacheFile))
+
+	// Check that the file was created
+	_, err = os.Stat(client.cacheFile)
+	assert.NoError(t, err)
+
+	// Clean up the temporary file
+	err = os.Remove(client.cacheFile)
+	assert.NoError(t, err)
+}
+
+func TestNewSofaClient(t *testing.T) {
+	client, err := NewSofaClient()
+	assert.NoError(t, err)
+	assert.Equal(t, SofaV1URL, client.endpoint)
+	assert.NotNil(t, client.httpClient)
+}
+
+func TestWithHTTPClient(t *testing.T) {
+	client, err := NewSofaClient(WithHTTPClient(&http.Client{}))
+	assert.NoError(t, err)
+	assert.NotNil(t, client.httpClient)
+}
+
+func TestWithEndpoint(t *testing.T) {
+	client, err := NewSofaClient(WithURL("http://example.com"))
+	assert.NoError(t, err)
+	assert.Equal(t, "http://example.com", client.endpoint)
+}
+
+func TestSetCachePaths(t *testing.T) {
+	client, err := NewSofaClient()
+	assert.NoError(t, err)
+
+	client.setCachePaths()
+
+	assert.NotEmpty(t, client.cacheFile)
+	assert.NotEmpty(t, client.etagFile)
+}
+
+func TestCachePath(t *testing.T) {
+	client, err := NewSofaClient()
+	assert.NoError(t, err)
+
+	client.setCachePaths()
+
+	assert.NotEmpty(t, client.cachePath("test"))
+}
+
+func TestEtagPath(t *testing.T) {
+	client, err := NewSofaClient()
+	assert.NoError(t, err)
+
+	client.setCachePaths()
+
+	assert.NotEmpty(t, client.etagPath("test"))
+}
+
+func TestCreateCacheDir(t *testing.T) {
+	client, err := NewSofaClient()
+	assert.NoError(t, err)
+
+	err = client.createCacheDir()
+	assert.NoError(t, err)
+
+	assert.DirExists(t, client.cacheDir)
+}
