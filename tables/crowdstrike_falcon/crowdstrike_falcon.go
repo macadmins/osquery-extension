ca615bd4d953cbacf79ac276aa26ef5623f443c9
jc0b <j@jc0b.computer>
2025-10-08 22:04:22 +0200

    Bumped version, added some Linux support for Crowdstrike
    # Conflicts:
    #	VERSION

--- tables/crowdstrike_falcon/crowdstrike_falcon.go
+++ tables/crowdstrike_falcon/crowdstrike_falcon.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"regexp"
+	"runtime"
 	"strconv"
+	"strings"
 
 	"github.com/groob/plist"
 	"github.com/macadmins/osquery-extension/pkg/utils"
@@ -12,7 +15,10 @@ import (
 	"github.com/pkg/errors"
 )
 
-const falconCtlPath = "/Applications/Falcon.app/Contents/Resources/falconctl"
+var falconCtlPath = map[string]string{
+	"linux":  "/opt/CrowdStrike/falconctl",
+	"darwin": "/Applications/Falcon.app/Contents/Resources/falconctl",
+}
 
 type CrowdStrikeOutput struct {
 	AgentID                  string `plist:"aid"`
@@ -56,19 +62,51 @@ func CrowdstrikeFalconGenerate(ctx context.Context, queryContext table.QueryCont
 func runCrowdstrikeFalcon(r utils.Runner, fs utils.FileSystem) (CrowdStrikeOutput, error) {
 	var output CrowdStrikeOutput
 
-	_, err := fs.Stat(falconCtlPath)
+	_, err := fs.Stat(falconCtlPath[runtime.GOOS])
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
 			return output, nil
 		}
 		return output, err
 	}
-	out, err := r.Runner.RunCmd(falconCtlPath, "info")
-	if err != nil {
-		return output, errors.Wrap(err, falconCtlPath+" info")
-	}
-	if err := plist.Unmarshal(out, &output); err != nil {
-		return output, errors.Wrap(err, "unmarshalling falconctl output")
+	switch runtime.GOOS {
+	case "darwin":
+		out, err := r.Runner.RunCmd(falconCtlPath[runtime.GOOS], "info")
+		if err != nil {
+			return output, errors.Wrap(err, falconCtlPath[runtime.GOOS]+" info")
+		}
+		if err := plist.Unmarshal(out, &output); err != nil {
+			return output, errors.Wrap(err, "unmarshalling falconctl output")
+		}
+	case "linux":
+		out, err := r.Runner.RunCmd(falconCtlPath[runtime.GOOS], "-g", "--aid")
+		if err != nil {
+			return output, errors.Wrap(err, falconCtlPath[runtime.GOOS]+" -g --aid")
+		}
+		agentIdRegex := regexp.MustCompile(`[a-f0-9]{16}`)
+		output.AgentID = agentIdRegex.FindStringSubmatch(strings.ToLower(string(out)))[1]
+
+		out, err = r.Runner.RunCmd(falconCtlPath[runtime.GOOS], "-g", "--cid")
+		if err != nil {
+			return output, errors.Wrap(err, falconCtlPath[runtime.GOOS]+" -g --cid")
+		}
+		cidRegex := regexp.MustCompile(`[a-f0-9]{16}`)
+		output.CID = cidRegex.FindStringSubmatch(strings.ToLower(string(out)))[1]
+
+		out, err = r.Runner.RunCmd(falconCtlPath[runtime.GOOS], "-g", "--rfm-state")
+		if err != nil {
+			return output, errors.Wrap(err, falconCtlPath[runtime.GOOS]+" -g --rfm-state")
+		}
+		rfmState := strings.Split(strings.ToLower(string(out)), "=")[1]
+		output.ReducedFunctionalityMode = rfmState == "true"
+
+		out, err = r.Runner.RunCmd(falconCtlPath[runtime.GOOS], "-g", "--version")
+		if err != nil {
+			return output, errors.Wrap(err, falconCtlPath[runtime.GOOS]+" -g --version")
+		}
+		version := strings.TrimSpace(strings.Split(strings.ToLower(string(out)), "=")[1])
+		output.FalconVersion = version
+
 	}
 
 	return output, nil
