4eb478f1964323de733ee6247f033e41c2df4d15
Graham Gilbert <graham.gilbert@airbnb.com>
2026-05-13 22:37:34 -0700

    Improve SOFA table tests

--- tables/sofa/sofa_cves_test.go
+++ tables/sofa/sofa_cves_test.go
@@ -3,8 +3,19 @@ package sofa
 import (
 	"testing"
 
+	"github.com/osquery/osquery-go/plugin/table"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestSofaUnpatchedCVEsColumns(t *testing.T) {
+	assert.Equal(t, []table.ColumnDefinition{
+		table.TextColumn("os_version"),
+		table.TextColumn("cve"),
+		table.TextColumn("patched_version"),
+		table.TextColumn("actively_exploited"),
+		table.TextColumn("url"),
+	}, SofaUnpatchedCVEsColumns())
+}
 
 func TestGetUnpatchedCVEs(t *testing.T) {
 	tests := []struct {
@@ -57,6 +68,48 @@ func TestGetUnpatchedCVEs(t *testing.T) {
 				},
 			},
 			osVersion: "10.0",
+			wantCVEs:  []UnpatchedCVE{},
+			wantErr:   false,
+		},
+		{
+			name:      "invalid current version",
+			root:      Root{},
+			osVersion: "not-a-version",
+			wantCVEs:  []UnpatchedCVE{},
+			wantErr:   true,
+		},
+		{
+			name: "invalid patched version",
+			root: Root{
+				OSVersions: []OSVersion{
+					{
+						SecurityReleases: []SecurityRelease{
+							{
+								ProductVersion: "not-a-version",
+							},
+						},
+					},
+				},
+			},
+			osVersion: "14.0",
+			wantCVEs:  []UnpatchedCVE{},
+			wantErr:   true,
+		},
+		{
+			name: "different major version ignored",
+			root: Root{
+				OSVersions: []OSVersion{
+					{
+						SecurityReleases: []SecurityRelease{
+							{
+								ProductVersion: "15.0",
+								CVEs:           map[string]bool{"CVE-9999": true},
+							},
+						},
+					},
+				},
+			},
+			osVersion: "14.6",
 			wantCVEs:  []UnpatchedCVE{},
 			wantErr:   false,
 		},
