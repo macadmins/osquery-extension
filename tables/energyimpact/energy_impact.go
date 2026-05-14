2519c85ec4fa9ab58a8a8f69656f31bfa9dc72f7
Graham Gilbert <graham.gilbert@airbnb.com>
2026-05-13 22:15:21 -0700

    Improve energy impact table tests

--- tables/energyimpact/energy_impact.go
+++ tables/energyimpact/energy_impact.go
@@ -21,25 +21,25 @@ type powermetricsOutput struct {
 
 // task represents individual process data from powermetrics
 type task struct {
-	PID                   int     `plist:"pid"`
-	Name                  string  `plist:"name"`
-	EnergyImpact          float64 `plist:"energy_impact"`
-	EnergyImpactPerS      float64 `plist:"energy_impact_per_s"`
-	CPUTimeNS             int64   `plist:"cputime_ns"`
-	CPUTimeMSPerS         float64 `plist:"cputime_ms_per_s"`
-	CPUTimeUserlandRatio  float64 `plist:"cputime_userland_ratio"`
-	IntrWakeups           int     `plist:"intr_wakeups"`
-	IntrWakeupsPerS       float64 `plist:"intr_wakeups_per_s"`
-	IdleWakeups           int     `plist:"idle_wakeups"`
-	IdleWakeupsPerS       float64 `plist:"idle_wakeups_per_s"`
-	DiskIOBytesRead       int64   `plist:"diskio_bytesread"`
-	DiskIOBytesReadPerS   float64 `plist:"diskio_bytesread_per_s"`
-	DiskIOBytesWritten    int64   `plist:"diskio_byteswritten"`
+	PID                    int     `plist:"pid"`
+	Name                   string  `plist:"name"`
+	EnergyImpact           float64 `plist:"energy_impact"`
+	EnergyImpactPerS       float64 `plist:"energy_impact_per_s"`
+	CPUTimeNS              int64   `plist:"cputime_ns"`
+	CPUTimeMSPerS          float64 `plist:"cputime_ms_per_s"`
+	CPUTimeUserlandRatio   float64 `plist:"cputime_userland_ratio"`
+	IntrWakeups            int     `plist:"intr_wakeups"`
+	IntrWakeupsPerS        float64 `plist:"intr_wakeups_per_s"`
+	IdleWakeups            int     `plist:"idle_wakeups"`
+	IdleWakeupsPerS        float64 `plist:"idle_wakeups_per_s"`
+	DiskIOBytesRead        int64   `plist:"diskio_bytesread"`
+	DiskIOBytesReadPerS    float64 `plist:"diskio_bytesread_per_s"`
+	DiskIOBytesWritten     int64   `plist:"diskio_byteswritten"`
 	DiskIOBytesWrittenPerS float64 `plist:"diskio_byteswritten_per_s"`
-	PacketsReceived       int     `plist:"packets_received"`
-	PacketsSent           int     `plist:"packets_sent"`
-	BytesReceived         int64   `plist:"bytes_received"`
-	BytesSent             int64   `plist:"bytes_sent"`
+	PacketsReceived        int     `plist:"packets_received"`
+	PacketsSent            int     `plist:"packets_sent"`
+	BytesReceived          int64   `plist:"bytes_received"`
+	BytesSent              int64   `plist:"bytes_sent"`
 }
 
 // EnergyImpactColumns returns the column definitions for the energy_impact table
@@ -70,9 +70,12 @@ func EnergyImpactColumns() []table.ColumnDefinition {
 
 // EnergyImpactGenerate generates the table data when queried
 func EnergyImpactGenerate(ctx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
-	var results []map[string]string
+	r := utils.NewRunner()
+	fs := utils.OSFileSystem{}
+	return generateWithRunner(queryContext, r, fs)
+}
 
-	// Get interval from WHERE clause, default to 1000ms
+func parseInterval(queryContext table.QueryContext) int {
 	interval := defaultInterval
 	if constraintList, present := queryContext.Constraints["interval"]; present {
 		for _, constraint := range constraintList.Constraints {
@@ -84,40 +87,48 @@ func EnergyImpactGenerate(ctx context.Context, queryContext table.QueryContext) 
 		}
 	}
 
-	r := utils.NewRunner()
-	fs := utils.OSFileSystem{}
+	return interval
+}
+
+func generateWithRunner(queryContext table.QueryContext, r utils.Runner, fs utils.FileSystem) ([]map[string]string, error) {
+	interval := parseInterval(queryContext)
 	tasks, err := runPowermetrics(r, fs, interval)
 	if err != nil {
 		fmt.Println(err)
-		return results, err
+		return nil, err
 	}
 
+	return buildOutput(tasks, interval), nil
+}
+
+func buildOutput(tasks []task, interval int) []map[string]string {
+	var results []map[string]string
 	for _, t := range tasks {
 		results = append(results, map[string]string{
-			"pid":                      strconv.Itoa(t.PID),
-			"name":                     t.Name,
-			"energy_impact":            fmt.Sprintf("%.2f", t.EnergyImpact),
-			"energy_impact_per_s":      fmt.Sprintf("%.2f", t.EnergyImpactPerS),
-			"cputime_ns":               strconv.FormatInt(t.CPUTimeNS, 10),
-			"cputime_ms_per_s":         fmt.Sprintf("%.2f", t.CPUTimeMSPerS),
-			"cputime_userland_ratio":   fmt.Sprintf("%.2f", t.CPUTimeUserlandRatio),
-			"intr_wakeups":             strconv.Itoa(t.IntrWakeups),
-			"intr_wakeups_per_s":       fmt.Sprintf("%.2f", t.IntrWakeupsPerS),
-			"idle_wakeups":             strconv.Itoa(t.IdleWakeups),
-			"idle_wakeups_per_s":       fmt.Sprintf("%.2f", t.IdleWakeupsPerS),
-			"diskio_bytesread":         strconv.FormatInt(t.DiskIOBytesRead, 10),
-			"diskio_bytesread_per_s":   fmt.Sprintf("%.2f", t.DiskIOBytesReadPerS),
-			"diskio_byteswritten":      strconv.FormatInt(t.DiskIOBytesWritten, 10),
+			"pid":                       strconv.Itoa(t.PID),
+			"name":                      t.Name,
+			"energy_impact":             fmt.Sprintf("%.2f", t.EnergyImpact),
+			"energy_impact_per_s":       fmt.Sprintf("%.2f", t.EnergyImpactPerS),
+			"cputime_ns":                strconv.FormatInt(t.CPUTimeNS, 10),
+			"cputime_ms_per_s":          fmt.Sprintf("%.2f", t.CPUTimeMSPerS),
+			"cputime_userland_ratio":    fmt.Sprintf("%.2f", t.CPUTimeUserlandRatio),
+			"intr_wakeups":              strconv.Itoa(t.IntrWakeups),
+			"intr_wakeups_per_s":        fmt.Sprintf("%.2f", t.IntrWakeupsPerS),
+			"idle_wakeups":              strconv.Itoa(t.IdleWakeups),
+			"idle_wakeups_per_s":        fmt.Sprintf("%.2f", t.IdleWakeupsPerS),
+			"diskio_bytesread":          strconv.FormatInt(t.DiskIOBytesRead, 10),
+			"diskio_bytesread_per_s":    fmt.Sprintf("%.2f", t.DiskIOBytesReadPerS),
+			"diskio_byteswritten":       strconv.FormatInt(t.DiskIOBytesWritten, 10),
 			"diskio_byteswritten_per_s": fmt.Sprintf("%.2f", t.DiskIOBytesWrittenPerS),
-			"packets_received":         strconv.Itoa(t.PacketsReceived),
-			"packets_sent":             strconv.Itoa(t.PacketsSent),
-			"bytes_received":           strconv.FormatInt(t.BytesReceived, 10),
-			"bytes_sent":               strconv.FormatInt(t.BytesSent, 10),
-			"interval":                 strconv.Itoa(interval),
+			"packets_received":          strconv.Itoa(t.PacketsReceived),
+			"packets_sent":              strconv.Itoa(t.PacketsSent),
+			"bytes_received":            strconv.FormatInt(t.BytesReceived, 10),
+			"bytes_sent":                strconv.FormatInt(t.BytesSent, 10),
+			"interval":                  strconv.Itoa(interval),
 		})
 	}
 
-	return results, nil
+	return results
 }
 
 // runPowermetrics executes the powermetrics command and parses the output
